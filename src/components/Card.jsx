2c0d0064df69b4766c56d6a4f31ba5f304790231
Elena <www.azeyka@mail.ru>
2020-01-08 14:46:21 +0300

    внесены правки

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,7 @@
-import React from 'react'
+import React from "react";
 
 function Card(props) {
-    return (
-        <div className="card" style={{width: '18rem'}}>
-            {props.children}
-        </div>
-    )
+  return <div className="card">{props.children}</div>;
 }
 
-export default Card
-
+export default Card;
--- src/components/CardsCollection.jsx
+++ src/components/CardsCollection.jsx
@@ -1,25 +1,111 @@
-import React from 'react'
-import Card from './Card';
-import CardBody from './CardBody';
-import CardImage from './CardImage';
+import React from "react";
+import Card from "./Card";
+import CardBody from "./CardBody";
+import CardImage from "./CardImage";
 
 export default function CardsCollection() {
-    return (
-        <div className='cards_collection'>
-            <Card>
-                <CardImage src="https://preview.ibb.co/gLbbM8/our-team-person.jpg" name="funny boy"/>
-                <CardBody title="Some title">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <button href="#" className="btn btn-primary">Go somewhere</button>
-                </CardBody>
-            </Card>
+  return (
+    <div className="card-columns">
+      <Card>
+        <CardImage
+          src="https://preview.ibb.co/gLbbM8/our-team-person.jpg"
+          name="funny boy"
+        />
+        <CardBody title="Card with image">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-primary">
+            Go somewhere
+          </a>
+        </CardBody>
+      </Card>
 
-            <Card>
-                <CardBody title="Another title">
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <button href="#" className="btn btn-primary">Go somewhere</button>
-                </CardBody>
-            </Card>
-        </div>
-    )
+      <Card>
+        <CardBody title="Just text card">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-warning">
+            Go somewhere
+          </a>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardBody title="Just text card">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" class="card-link">
+            Ссылка карты
+          </a>
+          <a href="/" class="card-link">
+            Другая ссылка
+          </a>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardImage
+          src="https://image.ibb.co/e3FE9J/portfolio-clever-poster.png"
+          name="funny boy"
+        />
+        <CardBody title="Card with image">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-success">
+            Go somewhere
+          </a>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardBody title="Just text card">
+          <p className="card-text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam alias
+            praesentium quod et qui cupiditate, expedita asperiores dolore sint
+            veniam commodi ea porro accusantium voluptatum vero repudiandae
+            quaerat, veritatis nobis.
+          </p>
+          <a href="/" className="btn btn-danger">
+            Go somewhere
+          </a>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardBody title="Just text card">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-secondary">
+            Go somewhere
+          </a>
+        </CardBody>
+      </Card>
+
+      <Card>
+        <CardImage
+          src="https://i.ibb.co/JHBRbjz/some-image.jpg"
+          name="funny boy"
+        />
+        <CardBody title="Card with image">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="/" className="btn btn-info">
+            Go somewhere
+          </a>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
