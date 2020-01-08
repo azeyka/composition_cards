1213f02d57922cbf951a7ee71bbd01c5eee4e96a
Elena <www.azeyka@mail.ru>
2020-01-08 14:46:31 +0300

    Create Main.jsx

---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import CardsCollection from "./CardsCollection";
+
+export default function Main() {
+  return (
+    <div className="container mt-5">
+      <CardsCollection />
+    </div>
+  );
+}
